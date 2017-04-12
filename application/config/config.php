d05a65ce2a0abac836d02a0379c31335768230ff
Eduardo2505 <eduardopadillacz@gamail.com>
2017-04-12 16:53:37 -0500

    url

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://asianbistro.pvessy.com/AsianBistro/';
+$config['base_url']	= 'http://asianbistro.pvessy.com/AsianBistroDemo/';
 
 /*
 |--------------------------------------------------------------------------
